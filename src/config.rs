340667117104ffa99a27f2235618c354012daf68
Rodrigo Oliveira <rodrigo.dealer@gmail.com>
2020-04-13 17:27:12 +0200

    Reading yaml file

---
+++ src/config.rs
@@ -0,0 +1,52 @@
+use std::fs::File;
+use std::io::Read;
+
+#[derive(Debug, PartialEq, Serialize, Deserialize)]
+pub struct Config {
+    pub config: Data,
+}
+
+#[derive(Debug, PartialEq, Serialize, Deserialize)]
+pub struct Data {
+    provider: Provider,
+
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub environment: Option<Vec<String>>,
+}
+
+pub fn load_config() -> Option<Config> {
+    let filename = "config.yaml";
+    match File::open(filename) {
+        Ok(mut file) => {
+            let mut content = String::new();
+            file.read_to_string(&mut content).unwrap();
+
+            let application_data: Config = serde_yaml::from_str(&content).unwrap();
+            return Some(application_data)
+        }
+        Err(error) => {
+            println!("There is an error {}: {}", filename, error);
+            return None
+        }
+    }
+}
+
+#[derive(Debug, PartialEq, Serialize, Deserialize)]
+struct Provider {
+    name: String,
+    token: String,
+    url: String,
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_it_loads_config_file() {
+        let config = load_config();
+
+        assert_eq!(config.is_some(), true);
+        assert_eq!(config.unwrap().config.environment.unwrap().len(), 2);
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
+use std::env;
+#[macro_use]
+extern crate serde_derive;
+extern crate serde_yaml;
+
+mod config;
+
 fn main() {
-    println!("Hello, world!");
+    config::load_config();
+}
+
+fn set_var(name: &str, value: &str) {
+    env::set_var(name, value);
+}
+
+#[cfg(test)]
+mod tests {
+    use std::env;
+    use super::*;
+
+    #[test]
+    fn test_it_sets_environment_variable() {
+        let key = "MY_KEY";
+        let value = "MY_VALUE";
+
+        set_var(key, value);
+
+        assert_eq!(env::var(key), Ok(value.to_string()));
+    }
 }

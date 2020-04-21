f7362e98f14195f8d8841af181f84f241b58985d
Rodrigo Oliveira <rodrigo.oliveira@secretescapes.com>
2020-04-21 14:54:35 +0200

    More stuff

--- src/environment.rs
+++ src/environment.rs
@@ -1,13 +1,29 @@
 use std::env;
+use std::collections::HashMap;
+use crate::config::Config;
 
 fn set_var(name: &str, value: &str) {
     env::set_var(name, value);
+}
+
+fn get_envs(config: Config) -> HashMap<String, String> {
+    let mut envs = HashMap::new();
+        let environment = config.config.environment.unwrap();
+        for item in environment.iter() {
+            let item_split : Vec<&str> = item.split("=").collect();
+            envs.insert(item_split[0].to_string(), item_split[1].to_string());
+        }
+        envs
 }
 
 #[cfg(test)]
 mod tests {
     use std::env;
     use super::*;
+
+    use crate::config::Config;
+    use crate::config::Provider;
+    use crate::config::Data;
 
     #[test]
     fn test_it_sets_environment_variable() {
@@ -18,5 +34,23 @@ mod tests {
 
         assert_eq!(env::var(key), Ok(value.to_string()));
     }
-}
 
+    #[test]
+    fn test_it_matches_vault() {
+        let environment = vec!["ONE_ENV=fake".to_string(), "TWO_ENV=not_fake".to_string()];
+        let config = Config{config: Data{ environment: Some(environment), provider: Provider{ name: "Name".to_string(), token: "".to_string(), url: "".to_string() }}};
+
+        // let mut envs = HashMap::new();
+        // let bla = config.config.environment.unwrap();
+        // for item in bla.iter() {
+        //     let bla : Vec<&str> = item.split("=").collect();
+        //     envs.insert(bla[0].to_string(), bla[1].to_string());
+        // }
+
+        let envs = get_envs(config);
+
+        assert_eq!(2, envs.len());
+        assert_eq!(&"fake".to_string(), envs.get("ONE_ENV").unwrap());
+        assert_eq!(&"not_fake".to_string(), envs.get("TWO_ENV").unwrap());
+    }
+}

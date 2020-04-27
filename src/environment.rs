60906d56289a2bfc33a0380d47a121b753997c92
Rodrigo Oliveira <rodrigo.oliveira@secretescapes.com>
2020-04-27 12:16:58 +0200

    Refactoring

--- src/environment.rs
+++ src/environment.rs
@@ -1,19 +1,19 @@
 use std::env;
 use std::collections::HashMap;
-use crate::config::Config;
+// use crate::config::Config;
 
 fn set_var(name: &str, value: &str) {
     env::set_var(name, value);
 }
 
-fn get_envs(config: Config) -> HashMap<String, String> {
-    let mut envs = HashMap::new();
-        let environment = config.config.environment.unwrap();
-        for item in environment.iter() {
-            let item_split : Vec<&str> = item.split("=").collect();
-            envs.insert(item_split[0].to_string(), item_split[1].to_string());
+pub fn get_envs(envs: Option<Vec<String>>) -> HashMap<String, String> {
+    let mut environments = HashMap::new();
+        for item in envs.iter() {
+            let item_split : Vec<&str> = item[0].split("=").collect();
+            environments.insert(item_split[0].to_string(), item_split[1].to_string());
         }
-        envs
+
+        environments
 }
 
 #[cfg(test)]
@@ -21,9 +21,9 @@ mod tests {
     use std::env;
     use super::*;
 
-    use crate::config::Config;
-    use crate::config::Provider;
-    use crate::config::Data;
+    // use crate::config::Config;
+    // use crate::config::Provider;
+    // use crate::config::Data;
 
     #[test]
     fn test_it_sets_environment_variable() {
@@ -37,10 +37,9 @@ mod tests {
 
     #[test]
     fn test_it_matches_vault() {
-        let environment = vec!["ONE_ENV=fake".to_string(), "TWO_ENV=not_fake".to_string()];
-        let config = Config{config: Data{ environment: Some(environment), provider: Provider{ name: "Name".to_string(), token: "".to_string(), address: "".to_string() }}};
+        let environment = Some(vec!["ONE_ENV=fake".to_string(), "TWO_ENV=not_fake".to_string()]);
 
-        let envs = get_envs(config);
+        let envs = get_envs(environment);
 
         assert_eq!(2, envs.len());
         assert_eq!(&"fake".to_string(), envs.get("ONE_ENV").unwrap());
--- src/provider/vault.rs
+++ src/provider/vault.rs
@@ -5,26 +5,23 @@ pub mod vault {
     use crate::config::Config;
     use std::collections::HashMap;
 
+    use crate::environment::get_envs as config_envs;
+
     pub fn get_envs(c: Config) -> String {
         let host : &str = &c.config.provider.address;
+        let envs = c.config.environment;
         #[cfg(not(test))]
         let client = vault_api::Client::new(host, c.config.provider.token).unwrap();
         let mut secrets = HashMap::<String, String>::new();
-
-        for item in c.config.environment.unwrap() {
-            let vec: Vec<&str> = item.split('=').collect();
-            let var_name = vec.first().unwrap().to_string();
-            let secret_name = vec.last().unwrap();
-            #[cfg(not(test))]
-            let secret = client.get_secret(secret_name).unwrap();
-            #[cfg(not(test))]
-            secrets.insert(var_name, secret);
+        let config_envs = config_envs(envs);
+        for item in config_envs.keys() {
+            let secret = client.get_secret(config_envs.get(item).unwrap()).unwrap();
+            secrets.insert(item.to_string(), secret);
         }
 
         for (var, value) in secrets.iter() {
             println!("Calling {}: {}", var, value);
         }
-
 
         "vault".to_string()
     }
